a0ba50b8fefacdf9b6793e4877502f5d25d87b3f
Hubert Sleszynski <h.r.sleszynski@gmail.com>
2024-04-16 13:00:19 +0200

    added sidebar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { SidebarContext } from "../contexts/SidebarContext";
+import { BsBag } from "react-icons/bs";
 
 const Header = () => {
-  return <div>Header</div>;
+  const { setIsOpen, isOpen } = useContext(SidebarContext);
+  return (
+    <header>
+      <div>Header</div>
+      <div
+        className="cursor-pointer flex relative"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <BsBag className="text-2xl " />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
