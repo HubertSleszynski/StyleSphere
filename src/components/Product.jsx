782d69c332b0cee9767ba6f12ae14387244fe7bc
Hubert Sleszynski <h.r.sleszynski@gmail.com>
2024-04-17 16:09:42 +0200

    changes in optimization

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -13,13 +13,16 @@ const Product = ({ product }) => {
     <div>
       <div className="border border-[#e4e4e4] h-[300px] mb-4 relative overflow-hidden group tranistion">
         <div className="w-full h-full flex justify-center items-center">
-          <div className="w-[200px] mx-auto flex justify-center items-center">
+          <Link
+            to={`/product/${id}`}
+            className="w-[200px] mx-auto flex justify-center items-center"
+          >
             <img
               className="max-h-[160px] group-hover:scale-110 transition duration-300"
               src={image}
               alt=""
             />
-          </div>
+          </Link>
         </div>
 
         <div className="absolute top-6 -right-11 group-hover:right-5 p-2 flex flex-col items-center justify-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-300">
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -15,7 +15,7 @@ const Sidebar = () => {
         isOpen ? "right-0" : "-right-full"
       } w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}
     >
-      <div className="flex items-center justify-between py-6 border-b">
+      <div className="flex items-center justify-between py-4 border-b">
         <div className="uppercase text-sm font-semibold">
           Shopping Bag ({itemAmount})
         </div>
@@ -31,7 +31,7 @@ const Sidebar = () => {
           return <CartItem item={item} key={item.id} />;
         })}
       </div>
-      <div className="flex flex-col gap-y-3 py-4 mt-4">
+      <div className="flex flex-col gap-y-3 mt-4">
         <div className="flex w-full justify-between items-center">
           <div className="uppercase font-semibold">
             <span className="mr-2">total:</span> ${" "}
